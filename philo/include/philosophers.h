d114c443131b66f2d58c02c9a21f3f95c0aaae44
silvagomez <silva.camilo@gmail.com>
2023-10-26 12:10:56 +0200

    feat: creating strcutures

--- philo/include/philosophers.h
+++ philo/include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: dsilva-g <dsilva-g@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 22:34:38 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/10/24 23:39:31 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/10/25 23:39:51 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,26 @@
 
 # include "error.h"
 
+
+// fork?
+typedef struct	s_philo
+{
+	size_t	philo_id;
+	size_t	l_hand;
+	size_t	r_hand;	
+	t_data	*data;
+}				t_philo;
+
+typedef struct	s_data
+{
+	t_philo	*philo;
+	size_t	n_philos;
+	size_t	life_time;
+	size_t	eat_time;
+	size_t	zzz_time;
+	size_t	n_meals;
+}
+
 // error
 int		error_terminate(char *s);
 
@@ -31,5 +51,7 @@ int		validate_arg(char *ar[]);
 void	ft_putendl_fd(char *s, int fd);
 int		ft_isdigit(int c);
 int		ft_atoi(const char *str);
+
+// philosophers
 
 #endif
--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dsilva-g <dsilva-g@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/23 22:30:36 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/10/25 00:00:20 by dsilva-g         ###   ########.fr       */
+/*   Created: 2023/10/25 00:00:43 by dsilva-g          #+#    #+#             */
+/*   Updated: 2023/10/25 00:40:58 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	main(int ac, char *ar[])
+void	philosopher(char *arg[])
 {
-	if (ac < 5 || ac > 6)
-		return (error_terminate(ERR_ARG));
-	if (validate_arg(ar) < 0)
-		return (-1);
-	philosopher(ar);
-	return (0);
+	t_
 }
