b1ffd1a844aea4bbb89edc9cfe9d682f1abc9049
silvagomez <silva.camilo@gmail.com>
2023-12-17 12:25:30 +0100

    philos setting up ok

--- philo/include/error.h
+++ philo/include/error.h
@@ -6,7 +6,7 @@
 /*   By: dsilva-g <dsilva-g@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 23:18:45 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/11/02 11:05:05 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/12/17 12:20:51 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,26 @@
 # define ERR_JTH "ERROR: calling wait thread."
 # define ERR_MTH "ERROR: malloc thread."
 # define ERR_MMT "ERROR: malloc mutex."
+
+//COLOR
+#define BLK		"\033[30m"
+#define RED		"\033[31m"
+#define GRN		"\033[32m"
+#define YEL		"\033[33m"
+#define BLU		"\033[34m"
+#define MAG		"\033[35m"
+#define CYN		"\033[36m"
+#define WHT		"\033[37m"
+#define RST		"\033[0m"
+
+//HIGH COLOR
+#define HBLK	"\033[90m"
+#define HRED	"\033[91m"
+#define HGRN	"\033[92m"
+#define HYEL	"\033[93m"
+#define HBLU	"\033[94m"
+#define HMAG	"\033[95m"
+#define HCYN	"\033[96m"
+#define HWHT	"\033[97m"
 
 #endif
